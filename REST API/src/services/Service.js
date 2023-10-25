b45c231319890dddbdf62e86faaa8254ce9284e2
Steliyan Petkov <stspsp@gmail.com>
2023-10-26 00:24:04 +0300

    REST pagination added

--- REST API/src/services/Service.js
+++ REST API/src/services/Service.js
@@ -1,33 +1,40 @@
 const CustomError = require("../utilities/CustomError");
 
 class Service {
-    constructor(model) {
-      this.model = model;
-    }
-  
-    async getAll(query) {
-      return await this.model.find(query || {});
-    }
-  
-    async getById(id) {
-      return await this.model.findById(id);
-    }
-  
-    async create(input) {
-      return await this.model.create(input);
-    }
-  
-    async update(id, input) {
-      const entity = await this.model.findById(id);
-      if(entity === null) throw new CustomError('Entity not found', 404)
-  
-      Object.assign(entity, input);
-      return await entity.save();
-    }
-  
-    async deleteById(id) {
-      return await this.model.deleteOne({_id: id});
-    }
+  constructor(model) {
+    this.model = model;
   }
-  
-  module.exports = Service;
+
+  async getAll(query) {
+    const { page = 1, limit = 1, ...filters } = query;
+    const count = await this.model.countDocuments(filters);
+    const totalPages = Math.ceil(count / limit);
+    const nextPage = +page < totalPages ? +page + 1 : null;
+    const prevPage = +page > 1 ? +page - 1 : null;
+
+    const results = await this.model.find(filters || {});
+    return { results, totalPages, currentPage: page, nextPage, prevPage }
+  }
+
+  async getById(id) {
+    return await this.model.findById(id);
+  }
+
+  async create(input) {
+    return await this.model.create(input);
+  }
+
+  async update(id, input) {
+    const entity = await this.model.findById(id);
+    if (entity === null) throw new CustomError('Entity not found', 404)
+
+    Object.assign(entity, input);
+    return await entity.save();
+  }
+
+  async deleteById(id) {
+    return await this.model.deleteOne({ _id: id });
+  }
+}
+
+module.exports = Service;
--- REST API/src/services/positionService.js
+++ REST API/src/services/positionService.js
@@ -4,40 +4,57 @@ const isAuthorizedUser = require("../utilities/isAuthorizedUser");
 const Service = require("./Service");
 
 class PositionService extends Service {
-    constructor() {
-      super(Position);
+  constructor() {
+    super(Position);
+  }
+
+  // Override limiting user role
+  async getAll(user, query) {
+    // pagination
+    const { page = 1, limit = 1, ...filters } = query;
+    const totalPages =await  this.model.countDocuments(filters);
+    const nextPage = +page < totalPages ? +page + 1 : null;
+    const prevPage = +page > 1 ? +page - 1 : null;
+    let results;
+
+    if (user.role === 'user') {
+      results =  await this.model.find(filters)
+        .limit(+limit)
+        .skip((page - 1) * limit)
+        .select('-salary')
+        .populate('employee department');
     }
 
-    // Override limiting user role
-    async getAll(user, query) {
-      if(user.role === 'user') {
-        return await this.model.find(query ).select('-salary').populate('department');
-      }
-      return await this.model.find(query).populate('employee department salary');
-    }
+    results = await this.model.find(filters)
+      .limit(+limit)
+      .skip((page - 1) * limit)
+      .populate('employee department salary');
 
-    // get by employee id
-    async getById(employee,user) {
-      console.log( await this.model.find({employee, active: true}));
-      const position = await this.model.findOne({employee, active: true});
-      console.log(employee);
-      if(position == null) return null;
-      // Check and return partial data
-      if(!isAuthorizedUser(user.role, user.employeeId, position.employee)) {
-        return await this.model.findOne({employee, active: true}).select('-salary').populate('department');
-      }
-      return position.populate('employee department salary');
-    }
+      return { results, totalPages, currentPage: page, nextPage, prevPage }
+  }
 
-    async create(position) {
-      // Change recent active position to inactive and set end date
-      const recentPosition = await this.model.findOne({employee: position.employee, active: true}).exec();
-      recentPosition.active = false;
-      recentPosition.endDate = position.startDate;
-      recentPosition.save();
-
-      // Create new position
-      return await this.model.create(position);
+  // get by employee id
+  async getById(employee, user) {
+    console.log(await this.model.find({ employee, active: true }));
+    const position = await this.model.findOne({ employee, active: true });
+    console.log(employee);
+    if (position == null) return null;
+    // Check and return partial data
+    if (!isAuthorizedUser(user.role, user.employeeId, position.employee)) {
+      return await this.model.findOne({ employee, active: true }).select('-salary').populate('employee department');
     }
+    return position.populate('employee department salary');
+  }
+
+  async create(position) {
+    // Change recent active position to inactive and set end date
+    const recentPosition = await this.model.findOne({ employee: position.employee, active: true }).exec();
+    recentPosition.active = false;
+    recentPosition.endDate = position.startDate;
+    recentPosition.save();
+
+    // Create new position
+    return await this.model.create(position);
+  }
 }
 module.exports = new PositionService()
